3194e80b4641b343c514701e6b4eb27312ac1165
Damian Cejas <damianfcejas@gmail.com>
2022-09-30 14:03:43 -0300

    usuarios service creado y funcionando

--- src/app/pages/usuarios/usuarios.component.ts
+++ src/app/pages/usuarios/usuarios.component.ts
@@ -1,3 +1,4 @@
+import { UsuariosService } from './../../services/usuarios.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuariosComponent implements OnInit {
 
-  constructor() { }
+  constructor( private usuariosService: UsuariosService) { }
 
   ngOnInit(): void {
   }
---
+++ src/app/services/usuarios.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuariosService {
+
+  constructor() {
+    console.log('usuarios service listo')
+   }
+}
